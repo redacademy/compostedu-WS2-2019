84c022a82680b36c1b891636230c1dea6809e2b2
Omar Lomeli <o.a.1027@outlook.com>
2020-03-10 20:47:38 -0700

    #13 start to apply style on contact us finished header

--- themes/compostedu/page-contact-us.php
+++ themes/compostedu/page-contact-us.php
@@ -10,14 +10,16 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'contact-us' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
