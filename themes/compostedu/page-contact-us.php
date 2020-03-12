8bf0dd79c18ba44939e1f8cfba04298c6f89c5cf
Omar Lomeli <o.a.1027@outlook.com>
2020-03-11 22:29:16 -0700

    #13 add style to most of the page

--- themes/compostedu/page-contact-us.php
+++ themes/compostedu/page-contact-us.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="contact-us-site-main" role="main">
 
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -16,6 +16,16 @@ get_header(); ?>
 				<?php get_template_part( 'template-parts/content', 'contact-us' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
+			
+			<div class='subscribe-container'>
+				<form class='newsletter-subscription-form'>
+				<label for='name'>Name</label><br>
+				<input class='subscribe-input'  type="text" id="fname" name="fname"><br>
+				<label for='email'>Email Address</label><br>
+				<input class='subscribe-input'  type="text" id="fname" name="fname"><br>
+				<button class='subscribe-btn'>Subscribe</button>
+				</form>
+			</div>
 
 
 		</main><!-- #main -->
