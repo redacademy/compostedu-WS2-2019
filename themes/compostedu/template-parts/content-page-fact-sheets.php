642d5de57d2d3c8753942dd497b6c0564eafcd36
Valterfi <morpheuw@gmail.com>
2020-03-12 18:04:45 -0700

    #22 creating box style for fact sheets

--- themes/compostedu/template-parts/content-page-fact-sheets.php
+++ themes/compostedu/template-parts/content-page-fact-sheets.php
@@ -7,12 +7,15 @@
 
 ?>
 
+<?php $fact_sheets = get_field('fact_sheets'); ?>
+
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content-fs">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
@@ -21,15 +24,21 @@
 
 		<div class="type-fs-content">
 			<?php 
-				$fact_sheets = get_field('fact_sheets');
-
 				foreach($fact_sheets as $fact_sheet) {
 					echo '<a href=\'#\'>' . $fact_sheet['type'] . '</a>';
 				}
 			?>
-		<div>
+		</div>
+	</div>
 
-
-	<div>
+	<div class="entry-content">
+		<?php foreach($fact_sheets as $fact_sheet): ?>
+			<div class="box-fs">
+				<h1><?php echo $fact_sheet['type']?></h1>
+				<h2><?php echo $fact_sheet['title']?></h2>
+				<p><?php echo $fact_sheet['description']?></p>
+			</div>
+		<?php endforeach; ?>
+	</div>
 	
 </article><!-- #post-## -->
