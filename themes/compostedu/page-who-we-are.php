707ff8ed2e02100d0fd9f3e35256688004654c91
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-03-15 13:46:41 -0700

    #26 added who-we-are file to template-parts and added style to it

--- themes/compostedu/page-who-we-are.php
+++ themes/compostedu/page-who-we-are.php
@@ -8,11 +8,11 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="who-we-are-site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'who-we-are' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
