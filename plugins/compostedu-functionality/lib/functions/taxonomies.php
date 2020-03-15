a339d3e48e35638ad789c4bc1e341c14d50f0205
Valterfi <morpheuw@gmail.com>
2020-03-15 16:29:27 -0700

    #45 adding attribute show in rest for be possible fill product taxonomy in the product page on wordpress dashboard

--- plugins/compostedu-functionality/lib/functions/taxonomies.php
+++ plugins/compostedu-functionality/lib/functions/taxonomies.php
@@ -40,6 +40,7 @@ function product_taxonomy() {
 		'show_admin_column'          => true,
 		'show_in_nav_menus'          => true,
 		'show_tagcloud'              => true,
+		'show_in_rest'          	 => true,
 	);
 	register_taxonomy( 'product_taxonomy', array( 'product' ), $args );
 
