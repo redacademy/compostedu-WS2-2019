cbedd1b41716f569202d603c75e5aa8bdc17fcde
Omar Lomeli <o.a.1027@outlook.com>
2020-03-16 20:00:29 -0700

    #41 create template for volunteer and add content on admin

---
+++ themes/compostedu/template-parts/content-volunteer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="volunteer-page-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <p class ='volunteer-header-text'>Thank you for your interest in volunteering with the Compost Education Centre!  Our training sessions 
+            generally take place once a year over the course of a weekend. 
+             Find your volunteer opportunity now. </p>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
