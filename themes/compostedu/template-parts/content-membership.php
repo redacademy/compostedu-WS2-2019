e74addbf3841a5fad77d16256c0e57eb46548c2a
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-03-17 18:36:25 -0700

    added content

---
+++ themes/compostedu/template-parts/content-membership.php
@@ -0,0 +1,10 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="teaching-garden-page-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <p class ='header-description'>TAs a member of the Compost Education Centre, you will be supporting composting, soil conservation and organic gardening education programs while receiving benefits listed below.</p>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
