951b2759b676263e0273e3d98cf394072199a0e0
Valterfi <morpheuw@gmail.com>
2020-03-17 21:48:15 -0700

    #55 staring get news with pagination

---
+++ themes/compostedu/home.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<section class="news-short-content">
+				
+							
+
+		</section>
+
+		<section class="news-content">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'news' ); ?>
+
+				<?php endwhile; ?>
+
+				<?php red_starter_numbered_pagination(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; ?>
+
+		</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
