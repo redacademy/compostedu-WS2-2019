600ccd27c0af76d2b5ae6b8afd0957018da1a884
Omar Lomeli <o.a.1027@outlook.com>
2020-03-17 18:34:43 -0700

    #57 create page templates and add image folder

---
+++ themes/compostedu/template-parts/content-donate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="donate-page-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <p class ='donate-header-description'>We rely on people like you!
+It’s your goodwill and financial support that help produce the programs and services that make such a difference in our community.  
+Thank you so much! </p>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
