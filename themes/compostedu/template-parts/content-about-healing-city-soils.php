0e1b88b40116308f73f9a3aa2fc3653b0d6510b4
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-03-26 01:41:51 -0700

    #64 add style to mobile version

--- themes/compostedu/template-parts/content-about-healing-city-soils.php
+++ themes/compostedu/template-parts/content-about-healing-city-soils.php
@@ -13,8 +13,10 @@
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
 
-        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-        <p class="healind-soil-title-description">Thanks to generous funding from the Victoria Foundation, and the Capital Regional District, Healing City Soils (HCS), a partnership between the CEC, soil toxicologist and HCS Program Technician, Danielle Stevenson of DIY FUNGI, GIS specialist and Mapping Coordinator Steeve Deschenes, and Royal Roads University, enters its 5th year!</p>
+        <div class="text">
+            <h1 class="entry-title">Healing City Soils</h1>
+            <p class="healind-soil-title-description">Thanks to generous funding from the Victoria Foundation, and the Capital Regional District, Healing City Soils (HCS), a partnership between the CEC, soil toxicologist and HCS Program Technician, Danielle Stevenson of DIY FUNGI, GIS specialist and Mapping Coordinator Steeve Deschenes, and Royal Roads University, enters its 5th year!</p>
+        </div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
